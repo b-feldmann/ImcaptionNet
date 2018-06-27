6add0fd11bbd232b6378b4a28991796dcd7dbedd
Victor Künstler <victor.kuenstler@outlook.com>
2018-06-27 19:12:52 +0200

    Fix indent

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import numpy as np
 from imcaption_net import ImcaptionNet
 
 if __name__ == '__main__':
-  img_path = '.data/train2014/COCO_train2014_000000000009.jpg'
-  img = image.load_img(img_path, target_size=(224, 224))
-  x = image.img_to_array(img)
-  x = np.expand_dims(x, axis=0)
-  x = preprocess_input(x)
+    img_path = '.data/train2014/COCO_train2014_000000000009.jpg'
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
 
+    net = ImcaptionNet()
 
-  net = ImcaptionNet()
-
-  features = net.predict(x)
-  print(features.shape)
+    features = net.predict(x)
+    print(features.shape)
