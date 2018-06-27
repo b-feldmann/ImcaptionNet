91d8523a9457df48d7ed8e80f16a4584026012cf
Victor Künstler <victor.kuenstler@outlook.com>
2018-06-27 19:11:35 +0200

    Setup project

---
+++ imcaption_net.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow.python.keras.models import Model
+
+class ImcaptionNet(Model):
+  """docstring for ImcaptionNet"""
+
+  def __init__(self):
+    super().__init__(name='imcaption_net')
+    base_resnet50 = tf.keras.applications.resnet50.ResNet50(
+        include_top=False, weights='imagenet', input_tensor=None,
+        input_shape=None, pooling=None)
+    # EXPERIMENT: Testen ob wir die Activation (-2) oder das Pooling (-1) benutzen
+    self.input = base_resnet50.input
+    self.encoder = Model(
+        inputs=self.input, output=base_resnet50.layers[-2].output)
+
+  def build(self, input_shape):
+    # TODO Decoder (LSTM)
+    super().build(input_shape)
+
+  def call(self, inputs):
+    return self.encoder(inputs)
+
+  def compute_output_shape(self, input_shape):
+    pass
+
--- main.py
+++ main.py
@@ -1,34 +1,8 @@
-import tensorflow as tf
-
 from tensorflow.python.keras.preprocessing import image
 from tensorflow.python.keras.applications.resnet50 import preprocess_input
-from tensorflow.python.keras.models import Model
 import numpy as np
 
-
-class ImcaptionNet(Model):
-  """docstring for ImcaptionNet"""
-
-  def __init__(self):
-    super().__init__(name='imcaption_net')
-    base_resnet50 = tf.keras.applications.resnet50.ResNet50(
-        include_top=False, weights='imagenet', input_tensor=None,
-        input_shape=None, pooling=None)
-    # EXPERIMENT: Testen ob wir die Activation (-2) oder das Pooling (-1) benutzen
-    self.input = base_resnet50.input
-    self.encoder = Model(
-        inputs=self.input, output=base_resnet50.layers[-2].output)
-
-  def build(self, input_shape):
-    # TODO Decoder (LSTM)
-    super().build(input_shape)
-
-  def call(self, inputs):
-    return self.encoder(inputs)
-
-  def compute_output_shape(self, input_shape):
-    pass
-
+from imcaption_net import ImcaptionNet
 
 if __name__ == '__main__':
   img_path = '.data/train2014/COCO_train2014_000000000009.jpg'
