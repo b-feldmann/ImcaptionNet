09a38cab4333928d55826c72d81e2ff76af2b5a9
Victor Künstler <victor.kuenstler@outlook.com>
2018-06-28 14:00:01 +0200

    Add adaptive attention model

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-from tensorflow.python.keras.preprocessing import image
-from tensorflow.python.keras.applications.resnet50 import preprocess_input
+import click
 import numpy as np
+from tensorflow.python.keras.applications.resnet50 import preprocess_input
+from tensorflow.python.keras.preprocessing import image
 
-from imcaption_net import ImcaptionNet
+from adaptiveModel import Encoder2Decoder
 
-if __name__ == '__main__':
-    img_path = '.data/train2014/COCO_train2014_000000000009.jpg'
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--imsize', default=224, help='Image Size')
+@click.option('--vocab-path', help='Path to vocab')
+def train(imsize):
+    img_path = 'data/train2014/COCO_train2014_000000000009.jpg'
     img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
+    adaptive = Encoder2Decoder(256, len(vocab), 512)
 
-    net = ImcaptionNet()
 
-    features = net.predict(x)
-    print(features.shape)
+if __name__ == '__main__':
+    cli()
